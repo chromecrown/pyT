92283a6e742fc8537c1ca4bd9da43541fafa2274
chromecrown <chromecrown@hotmail.com>
2016-04-20 11:49:08 +0800

    #

--- com/xx/tst/util/__init__.py
+++ com/xx/tst/util/__init__.py
@@ -1,12 +1,33 @@
 #coding=utf-8
 
-def ifDictInstance(obj):
+def isDictInstance(obj):
     """
     是否字典类型
     """
     isDic = isinstance(obj,dict)
     return isDic
 
+def isTupleInstance(obj):
+    """
+    是否元组类型
+    """
+    isTuple = isinstance(obj,tuple)
+    return isTuple
+
+def isListInstance(obj):
+    """
+    是否列表类型
+    """
+    isList = isinstance(obj,list)
+    return isList
+
+def isIntInstance(obj):
+    """
+    是否int类型
+    """
+    isInt = isinstance(obj,int)
+    return isInt
+
 if __name__ == "__main__":
-    obj = {}
-    print ifDictInstance(obj)
+    obj = "12"
+    print isIntInstance(obj)
