448ee42c130cc829b3097a41ded1d537cded372a
chromecrown <chromecrown@hotmail.com>
2016-04-29 12:41:17 +0800

    some operation about string

--- com/xx/tst/str/StringT.py
+++ com/xx/tst/str/StringT.py
@@ -6,34 +6,57 @@ def getStrLength(s):
     """
     return len(s)
 
-def getIndex(s,substr):
+def testStr(s,substr):
     """
-    return the index in str if find
-    return -1 if not find 
     """
-    return s.find(substr)
-
-def replaceStr(s,old,new):
+    #字符串截取
+    print s[0:-1]
+    print s.find(substr)
+    print s.index(substr)
+    print s.rfind(substr)
+    s.count(substr)#计算substr在字符串中出现的次数
+    s.replace("old","new",1)#原来的字符串替换为新的字符串，最后的哦参数指定替换的次数
+    #s.strip([chars]),把s中前后chars中所有的字符串全部去掉
+    s.strip()
+    
+    
+    #字符串的分割和组合
+    s.split(",")
+    
+    #把指定的序列用给定的字符串连接起来
+    lst = ['a','b','c']
+    print s.join(lst)
+    
+    #字符串大小写
     """
-    S.replace(oldstr, newstr, [count])    
-    把S中的oldstr替换为newstr，count为替换次数
+    字符串中字符大小写的变换：
+    S.lower()   #小写 
+    S.upper()   #大写
+    S.swapcase()   #大小写互换 
+    S.capitalize()   #首字母大写 
+    String.capwords(S)  #这是模块中的方法。它把S用split()函数分开，然后用
+    capitalize()把首字母变成大写，最后用join()合并到一起
+    S.title()    #只有首字母大写，其余为小写，模块中没有这个方法
     """
-    return s.replace(old, new)
-
-def toLower(s):
+    
     """
-    小写
+    字符串的测试函数，这一类函数在string模块中没有，这些函数返回的都是bool值：
+    S.startwith(prefix[,start[,end]]) #是否以prefix开头 
+    S.endwith(suffix[,start[,end]])  #以suffix结尾 
+    S.isalnum()  #是否全是字母和数字，并至少有一个字符 
+    S.isalpha()  #是否全是字母，并至少有一个字符 
+    S.isdigit()  #是否全是数字，并至少有一个字符 
+    S.isspace() #是否全是空白字符，并至少有一个字符 
+    S.islower() #S中的字母是否全是小写 
+    S.isupper() #S中的字母是否便是大写 
+    S.istitle() #S是否是首字母大写的
     """
-    return s.lower()
-
-def toUpper(s):
-    """
-    小写
-    """
-    return s.upper()
+    
+    
+    return 
 
 if __name__ == "__main__":
     s = "springframework"
     substr = "@"
-    arg = getIndex(s, substr)
+    arg = getStrLength(s)
     print "arg:",arg
