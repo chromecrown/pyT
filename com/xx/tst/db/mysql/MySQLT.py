c4b19a0a98de65174714436ed3b2e91da343c843
chromecrown <chromecrown@hotmail.com>
2016-05-03 17:55:08 +0800

    #

--- com/xx/tst/db/mysql/MySQLT.py
+++ com/xx/tst/db/mysql/MySQLT.py
@@ -2,7 +2,6 @@
 
 import MySQLdb
 
-
 def getMySQLConn(host='10.150.140.108',usr='root',pwd='mysqlroot',dbname='weblog'):
     
     """
@@ -34,5 +33,58 @@ def getMySQLVer():
     
     return versionData 
 
+def createTbl():
+    """
+    test create table in mysql database
+    """
+    db = getMySQLConn(dbname="test")
+    cursor = db.cursor()
+    
+    cursor.execute("DROP TABLE IF EXISTS EMPLOYEE")
+    
+    sql = """
+        CREATE TABLE EMPLOYEE (
+         FIRST_NAME  CHAR(20) NOT NULL,
+         LAST_NAME  CHAR(20),
+         AGE INT,  
+         SEX CHAR(1),
+         INCOME FLOAT )
+         """
+    cursor.execute(sql)
+    closeConn(db)
 
-print "Database version is",getMySQLVer()
+def insertData():
+    """
+    test insert record to mysql
+    """
+    db = getMySQLConn(dbname="test")
+    cursor = db.cursor()
+    sql = """
+    INSERT INTO EMPLOYEE(FIRST_NAME,
+         LAST_NAME, AGE, SEX, INCOME)
+         VALUES ('Mac', 'Mohan', 20, 'M', 2000)
+    """
+    try:
+        cursor.execute(sql)
+        db.commit()
+    except Exception as e:
+        print(e)
+        db.rollback() 
+    finally:
+        closeConn(db)
+        
+def insertIPData(IPStr,getDay):
+    """
+    test insert record to mysql
+    """
+    db = getMySQLConn(dbname="weblog")
+    cursor = db.cursor()
+    sql = "INSERT INTO ip_req(hostip.getDay) VALUES (%s,%s)"%(IPStr,getDay)
+    try:
+        cursor.execute(sql)
+        db.commit()
+    except Exception as e:
+        print(e)
+        db.rollback() 
+    finally:
+        closeConn(db)
