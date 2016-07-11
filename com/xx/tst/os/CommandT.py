1256abe83b0ca1f150165ff6e83fd1805b2b7be7
chromecrown <chromecrown@hotmail.com>
2016-07-11 10:36:47 +0800

    use subprocess to call system command

--- com/xx/tst/os/CommandT.py
+++ com/xx/tst/os/CommandT.py
@@ -1,4 +1,9 @@
 #coding=utf-8
 
-import commands
+import subprocess
 
+cmd = "ping"
+p = subprocess.Popen(cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
+p.wait()
+
+print p.stdout.readlines()
